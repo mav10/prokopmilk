3c9d70168bb2c0a5e3b252aea782e4d6ab477140
mav10 <maxim.vasin@mail.ru>
2016-06-29 14:52:17 +0700

    Upload

---
+++ Models/Tovar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebSite.Models
+{
+    public class Tovar
+    {
+        [Key]
+        public int Tovar_ID { get; set; }
+        public string Category{ get; set; }
+        public float Volume { get; set; }
+        public char Dimension { get; set; }
+        public float Price { get; set; }
+        public string HOT { get; set; }
+        [Column(TypeName = "image")]
+        public byte[] Pict { get; set; }
+
+        //Ссылка на позиции заказа
+        public virtual List<OrderItem> OrderItems { get; set; }
+
+
+    }
+}
